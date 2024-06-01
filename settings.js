d035b0a9d84cdb8050a2329ce4e5d19dac4e61fc
BMFDEATH <111228525+BMFDEATH@users.noreply.github.com>
2024-06-01 03:12:06 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,15 +54,15 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['22569403821'] //ur owner number
+global.ownername = "᳀ᬊ𝐒𝚫𝚻𝚯𝚪𝐔𓅓𝐆𝚯𝐉𝚯ᬊ᳀" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
+global.owner = ['22569493821']
+global.ownertag = '22569403821' //ur tag number
 global.botname = 'Cheems Bot MD V4' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
